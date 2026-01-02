abec97a946b2e9d6959566a040795a10601c277c
Prabash89 <prabashonline@gmail.com>
2026-01-02 22:31:49 +0530

    Focus: core EKS cluster only

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,26 @@
-# Create the foundational VPC
+# terraform/main.tf
+# Provider Configuration
+terraform {
+  required_version = "~> 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # The backend is configured in backend.tf
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Data source for current AWS account
+data "aws_caller_identity" "current" {}
+
+# 1. VPC MODULE: Network Foundation
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -10,30 +32,29 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  enable_dns_hostnames   = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
 
   tags = var.tags
 }
 
-# Create the EKS cluster WITH add-ons using EKS Blueprints
-module "eks_blueprints" {
-  source  = "aws-ia/eks-blueprints/aws"
-  version = "~> 4.30" # Use the latest version
+# 2. EKS MODULE: Kubernetes Cluster
+module "eks" {
+  source  = "aws-ia/eks/aws"
+  version = "~> 1.0"
 
-  # Cluster details
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
 
-  # Network
-  vpc_id             = module.vpc.vpc_id
-  private_subnet_ids = module.vpc.private_subnets
+  # Link to the VPC
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
-  # Node Groups
+  # Node Group Configuration
   node_groups = {
-    managed_node_group = {
-      node_group_name = "managed-ondemand"
+    managed_ondemand = {
+      node_group_name = "managed_ondemand"
       instance_types  = ["t3.medium"]
       min_size        = 2
       max_size        = 5
@@ -42,32 +63,28 @@ module "eks_blueprints" {
     }
   }
 
-  # ==================== CORE ADD-ONS ====================
-  # This is where we declare what should be installed on the cluster.
-  # ArgoCD will be our GitOps operator.
-  platform_teams = {
-    admin = {
-      users = [data.aws_caller_identity.current.arn]
-    }
-  }
-
-  # Enable ArgoCD as the primary add-on
-  enable_argocd = true
-  argocd_helm_config = {
-    version = "5.46.7"
-    values  = [templatefile("${path.module}/argocd-values.yaml", {})]
-  }
-
-  # This application set will automatically bootstrap our App Config repo
-  argocd_applications = {
-    apps = {
-      path   = "chart"            # Path within the App Config Repo
-      repo_url = "https://github.com/Prabash89/eks-applications.git" # <<< UPDATE in Step 5
-    }
-  }
-
   tags = var.tags
 }
 
-# Fetch current AWS account ID for platform team mapping
-data "aws_caller_identity" "current" {}
+# 3. OUTPUTS: Information to access your cluster
+output "cluster_name" {
+  description = "The name of the created EKS cluster"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "The endpoint URL for your EKS cluster API server"
+  value       = module.eks.cluster_endpoint
+  sensitive   = true
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The base64-encoded certificate data required to communicate with your cluster"
+  value       = module.eks.cluster_certificate_authority_data
+  sensitive   = true
+}
+
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
